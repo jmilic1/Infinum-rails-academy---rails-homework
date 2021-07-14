422d39c72868839ff0367689b0acb4312674353c
jmilic1 <jm51506@fer.hr>
2021-07-14 15:57:45 +0200

    Add parsing of string to hash and filter nil values from parsed cities

--- app/domains/open_weather_map/open_weather_map.rb
+++ app/domains/open_weather_map/open_weather_map.rb
@@ -7,12 +7,12 @@ module OpenWeatherMap
     id = Resolver.city_id(name)
     return if id.nil?
 
-    data = HTTP.get(URL % [id, Rails.application.credentials[:open_weather_map_api_key]])
+    data = JSON.parse(HTTP.get(URL % [id, Rails.application.credentials[:open_weather_map_api_key]]))
 
     City.parse(data)
   end
 
   def self.cities(names)
-    names.map { |name| city(name) }
+    names.filter_map { |name| city(name) }
   end
 end
