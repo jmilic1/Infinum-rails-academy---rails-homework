69ef0f53d655adf48f6f0e2179ed8dfee5601bd1
jmilic1 <jm51506@fer.hr>
2021-07-14 13:28:18 +0200

    Fix Resolver and add Resolver tests

--- app/domains/open_weather_map/resolver.rb
+++ app/domains/open_weather_map/resolver.rb
@@ -1,10 +1,11 @@
 module OpenWeatherMap
   class Resolver
     def self.city_id(name)
-      data = JSON.parse(File.read(File.expand_path('city_ids.json', __dir__)))
-      result = data.find { |element| element['name'] == name }['id']
+      data = JSON.parse(File.read(File.expand_path("city.list.json", __dir__)))
+      entry = data.find { |element| element["name"] == name }
+
+      return if entry.nil?
+      entry
     end
-  end
-  def first_module_method
   end
 end
