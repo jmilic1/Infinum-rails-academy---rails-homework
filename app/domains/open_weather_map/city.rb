fffd5b3ea70f15965ff3412edda5f99ff9a1e5ad
jmilic1 <jm51506@fer.hr>
2021-07-14 14:25:13 +0200

    Change definition from "module OpenWeatherMap, class City" to "class OpenWeatherMap::City"

--- app/domains/open_weather_map/city.rb
+++ app/domains/open_weather_map/city.rb
@@ -1,37 +1,36 @@
-module OpenWeatherMap
-  class City
-    ZERO_CELSIUS_TO_KELVIN = 273.15
+class OpenWeatherMap::City
+  ZERO_CELSIUS_TO_KELVIN = 273.15
 
-    attr_reader :id, :lat, :lon, :name
+  attr_reader :id, :lat, :lon, :name
 
-    def initialize(id:, lat:, lon:, name:, temp_k:)
-      @id = id
-      @lat = lat
-      @lon = lon
-      @name = name
-      @temp_k = temp_k
-    end
-
-    def temp
-      @temp_k - ZERO_CELSIUS_TO_KELVIN
-    end
-
-    def <=>(other)
-      comparison = temp <=> other.temp
-      return comparison unless comparison == 0
-
-      name <=> other.name
-    end
-
-    def self.parse(data)
-      lat = data["coord"]["lat"]
-      lon = data["coord"]["lon"]
-      temp_k = data["main"]["temp"]
-      id = data["id"]
-      name = data["name"]
-
-      new(id: id, lat: lat, lon: lon, name: name, temp_k: temp_k)
-    end
-
+  def initialize(id:, lat:, lon:, name:, temp_k:)
+    @id = id
+    @lat = lat
+    @lon = lon
+    @name = name
+    @temp_k = temp_k
   end
+
+  def temp
+    @temp_k - ZERO_CELSIUS_TO_KELVIN
+  end
+
+  def <=>(other)
+    comparison = temp <=> other.temp
+    return comparison unless comparison == 0
+
+    name <=> other.name
+  end
+
+  def self.parse(data)
+    lat = data["coord"]["lat"]
+    lon = data["coord"]["lon"]
+    temp_k = data["main"]["temp"]
+    id = data["id"]
+    name = data["name"]
+
+    new(id: id, lat: lat, lon: lon, name: name, temp_k: temp_k)
+  end
+
 end
+
