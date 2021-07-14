4d3af3e6c1c6f61a6ef56b4931951bd3371ce3ed
jmilic1 <jm51506@fer.hr>
2021-07-14 14:26:13 +0200

    Change definition from "module OpenWeatherMap, class Resolver" to "module OpenWeatherMap::Resolver"

--- app/domains/open_weather_map/resolver.rb
+++ app/domains/open_weather_map/resolver.rb
@@ -1,11 +1,9 @@
-module OpenWeatherMap
-  class Resolver
-    def self.city_id(name)
-      data = JSON.parse(File.read(File.expand_path("city.list.json", __dir__)))
-      entry = data.find { |element| element["name"] == name }
+module OpenWeatherMap::Resolver
+  def self.city_id(name)
+    data = JSON.parse(File.read(File.expand_path("city.list.json", __dir__)))
+    entry = data.find { |element| element["name"] == name }
 
-      return if entry.nil?
-      entry
-    end
+    return if entry.nil?
+    entry
   end
 end
