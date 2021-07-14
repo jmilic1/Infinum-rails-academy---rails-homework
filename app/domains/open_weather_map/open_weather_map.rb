35f712ac30cce5cc56bd8699151838e901f700ed
jmilic1 <jm51506@fer.hr>
2021-07-14 15:02:53 +0200

    First implementation of open_weather_map.rb

---
+++ app/domains/open_weather_map/open_weather_map.rb
@@ -0,0 +1,14 @@
+require "http"
+
+URL = "https://samples.openweathermap.org/data/2.5/weather?id=%s&appid=%s"
+
+module OpenWeatherMap
+  def self.city(name)
+    id = Resolver.city_id(name)
+    return if id.nil?
+
+    data = HTTP.get(URL % [id, Rails.application.credentials[:open_weather_map_api_key]])
+
+    City.parse(data)
+  end
+end
