8b9f0424452eef5842fdda4f0a924fb3ba4ccef3
jmilic1 <jm51506@fer.hr>
2021-07-15 18:33:37 +0200

    Use HTTP.get() to insert parameters

--- app/domains/open_weather_map.rb
+++ app/domains/open_weather_map.rb
@@ -1,7 +1,6 @@
 require 'http'
 
-URL = 'https://api.openweathermap.org/data/2.5/weather?id=%s&appid=%s'
-URL_TEST = 'https://api.openweathermap.org/data/2.5/weather?id=%s&appid='
+URL = 'https://api.openweathermap.org/data/2.5/'
 
 module OpenWeatherMap
   def self.city(name)
@@ -9,11 +8,8 @@ module OpenWeatherMap
     return if id.nil?
 
     # rubocop:disable Layout/LineLength
-    # data = JSON.parse(HTTP.get(format(URL, id, Rails.application.credentials[:open_weather_map_api_key])))
-    data = JSON.parse(HTTP.get(format(URL_TEST, id) + Rails.application.credentials[:open_weather_map_api_key]))
+    City.parse(JSON.parse(HTTP.get("#{URL}weather", params: { id: id, appid: Rails.application.credentials[:open_weather_map_api_key] })))
     # rubocop:enable Layout/LineLength
-
-    City.parse(data)
   end
 
   def self.cities(names)
