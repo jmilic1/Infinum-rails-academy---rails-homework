adf5460689d7cafc5d24cdd8f94b8bdd4f34a0d6
jmilic1 <jm51506@fer.hr>
2021-07-15 12:17:00 +0200

    Rename json with city info

--- app/domains/open_weather_map/resolver.rb
+++ app/domains/open_weather_map/resolver.rb
@@ -1,7 +1,7 @@
 module OpenWeatherMap
   module Resolver
     def self.city_id(name)
-      data = JSON.parse(File.read(File.expand_path('city.list.json', __dir__)))
+      data = JSON.parse(File.read(File.expand_path('city_list.json', __dir__)))
       entry = data.find { |element| element['name'] == name }
 
       return if entry.nil?
