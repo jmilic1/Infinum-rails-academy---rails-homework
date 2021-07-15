5798f74201c26147c9a5eba488eac901a073ff28
jmilic1 <jm51506@fer.hr>
2021-07-15 12:16:41 +0200

    Test city.nearby format

--- app/domains/open_weather_map/city.rb
+++ app/domains/open_weather_map/city.rb
@@ -39,8 +39,11 @@ module OpenWeatherMap
 
     def nearby(count = 5)
       # rubocop:disable Layout/LineLength
-      data = JSON.parse(HTTP.get(format(URL, @name, count, Rails.application.credentials[:open_weather_map_api_key])))
+      # rubocop:disable Rails/Output
+      puts format(URL, name, count, Rails.application.credentials[:open_weather_map_api_key])
+      data = JSON.parse(HTTP.get(format(URL, name, count, Rails.application.credentials[:open_weather_map_api_key])))
       # rubocop:enable Layout/LineLength
+      # rubocop:enable Rails/Output
 
       data.map { |entry| OpenWeatherMap.city(entry['name']) }
     end
