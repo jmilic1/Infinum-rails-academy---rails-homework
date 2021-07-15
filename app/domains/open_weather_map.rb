cac19d7f0b59acb3ae674b9728839836229890ba
jmilic1 <jm51506@fer.hr>
2021-07-16 00:26:06 +0200

    Fix line too long error

--- app/domains/open_weather_map.rb
+++ app/domains/open_weather_map.rb
@@ -1,26 +1,19 @@
 require 'http'
 
 URL = 'https://api.openweathermap.org/data/2.5/'
+API_KEY = Rails.application.credentials[:open_weather_map_api_key]
 
 module OpenWeatherMap
   def self.city(name)
     id = Resolver.city_id(name)
     return if id.nil?
 
-    # rubocop:disable Layout/LineLength
-    City.parse(JSON.parse(HTTP.get("#{URL}weather", params: { id: id, appid: Rails.application.credentials[:open_weather_map_api_key] })))
-    # rubocop:enable Layout/LineLength
+    City.parse(JSON.parse(HTTP.get("#{URL}weather", params: { id: id, appid: API_KEY })))
   end
 
   def self.cities(names)
     ids = names.filter_map { |name| Resolver.city_id(name) }.join(',')
 
-    City.parse(
-      JSON.parse(
-        HTTP.get(
-          "#{URL}group", params: { id: ids, appid: Rails.application.credentials[:open_weather_map_api_key] }
-        )
-      )
-    )
+    City.parse(JSON.parse(HTTP.get("#{URL}group", params: { id: ids, appid: API_KEY })))
   end
 end
