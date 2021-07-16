7b908b3a0f9f51c36500847ee8964e282e13cfc9
jmilic1 <jm51506@fer.hr>
2021-07-16 23:38:37 +0200

    Correct grammar

--- spec/domains/open_weather_map/resolver_spec.rb
+++ spec/domains/open_weather_map/resolver_spec.rb
@@ -3,7 +3,7 @@ RSpec.describe OpenWeatherMap::Resolver do
     expect(described_class.city_id('Luxembourg')).not_to eq(nil)
   end
 
-  it 'returns nil of nonexistent city' do
+  it 'returns nil for a nonexistent city' do
     expect(described_class.city_id('a')).to eq(nil)
   end
 end
