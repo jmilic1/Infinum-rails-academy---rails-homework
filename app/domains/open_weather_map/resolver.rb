c947ccffd4ba59c21b911fab788b581912e152f9
jmilic1 <jm51506@fer.hr>
2021-07-16 22:55:42 +0200

    Delegate simple tasks to methods

--- app/domains/open_weather_map/resolver.rb
+++ app/domains/open_weather_map/resolver.rb
@@ -1,8 +1,19 @@
 module OpenWeatherMap
   module Resolver
+    def self.file_path
+      File.expand_path('city_list.json', __dir__)
+    end
+
+    def self.file
+      File.read(file_path)
+    end
+
+    def self.parsed_data(json)
+      JSON.parse(json)
+    end
+
     def self.city_id(name)
-      data = JSON.parse(File.read(File.expand_path('city_list.json', __dir__)))
-      entry = data.find { |element| element['name'] == name }
+      entry = parsed_data(file).find { |element| element['name'] == name }
 
       return if entry.nil?
 
