10c3819fe97e77af18d85cc9795ec0b6a84e3dc0
jmilic1 <jm51506@fer.hr>
2021-07-16 22:27:20 +0200

    Delegate simple tasks to new methods

--- app/domains/open_weather_map.rb
+++ app/domains/open_weather_map.rb
@@ -3,17 +3,32 @@ URL = 'https://api.openweathermap.org/data/2.5/'
 module OpenWeatherMap
   API_KEY = Rails.application.credentials[:open_weather_map_api_key]
 
+  def self.get_json(id, extension)
+    HTTP.get(URL + extension, params: { id: id, appid: API_KEY })
+  end
+
+  def self.get_weather_data(id)
+    JSON.parse(get_json(id, 'weather'))
+  end
+
+  def self.get_group_data(ids)
+    JSON.parse(get_json(ids, 'group'))
+  end
+
+  def self.get_city(id)
+    City.parse(get_weather_data(id))
+  end
+
   def self.city(name)
-    id = Resolver.city_id(name)
+    id = OpenWeatherMap::Resolver.city_id(name)
     return if id.nil?
 
-    City.parse(JSON.parse(HTTP.get("#{URL}weather", params: { id: id, appid: API_KEY })))
+    City.parse(get_weather_data(id))
   end
 
   def self.cities(names)
     ids = names.filter_map { |name| OpenWeatherMap::Resolver.city_id(name) }.join(',')
 
-    data = JSON.parse(HTTP.get("#{URL}group", params: { id: ids, appid: API_KEY }))
-    data['list'].map { |entry| City.parse(entry) }
+    get_group_data(ids)['list'].map { |entry| City.parse(entry) }
   end
 end
