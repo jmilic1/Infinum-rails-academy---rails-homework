127268d924cd1d8bf1cb969ec4ef1e06435e20bb
jmilic1 <jm51506@fer.hr>
2021-07-18 00:40:51 +0200

    Update booking model

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,11 +2,11 @@ class Booking < ApplicationRecord
   belongs_to :flight
   belongs_to :user
 
-  validates :seat_price, presence: true
-  validates :seat_price, numericality: { greater_than: 0 }
+  validates :seat_price, presence: true,
+                         numericality: { greater_than: 0 }
 
-  validates :no_of_seats, presence: true
-  validates :no_of_seats, numericality: { greater_than: 0 }
+  validates :no_of_seats, presence: true,
+                          numericality: { greater_than: 0 }
 
   validate :departs_at_after_now
   def departs_at_after_now
