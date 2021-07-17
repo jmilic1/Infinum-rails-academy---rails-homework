034ab059e9648e7b1a16a8ddcc737b8879a8c0b9
jmilic1 <jm51506@fer.hr>
2021-07-17 17:21:52 +0200

    Reorder methods

--- app/domains/open_weather_map/resolver.rb
+++ app/domains/open_weather_map/resolver.rb
@@ -1,19 +1,19 @@
 module OpenWeatherMap
   module Resolver
-    def self.file_path
-      File.expand_path('city_list.json', __dir__)
-    end
-
-    def self.file
-      File.read(file_path)
+    def self.city_id(name)
+      parsed_data(file).find { |element| element['name'] == name }&.dig('id')
     end
 
     def self.parsed_data(json)
       JSON.parse(json)
     end
 
-    def self.city_id(name)
-      parsed_data(file).find { |element| element['name'] == name }&.dig('id')
+    def self.file
+      File.read(file_path)
+    end
+
+    def self.file_path
+      File.expand_path('city_list.json', __dir__)
     end
   end
 end
