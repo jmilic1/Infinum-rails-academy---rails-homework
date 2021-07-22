d13f969e7cc936e8e0c28be45ce3496cf9108b0f
jmilic1 <jm51506@fer.hr>
2021-07-22 18:03:57 +0200

    Add belongs to associations to booking blueprinter serializer

--- app/serializers/booking_serializer.rb
+++ app/serializers/booking_serializer.rb
@@ -2,4 +2,7 @@ class BookingSerializer < Blueprinter::Base
   identifier :id
 
   fields :no_of_seats, :seat_price
+
+  association :projects, blueprint: FlightSerializer
+  association :users, blueprint: UserSerializer
 end
