c20a05baacd6661191c17479abda47786a0126e4
jmilic1 <jm51506@fer.hr>
2021-07-23 23:43:31 +0200

    Add roles to user

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -10,11 +10,12 @@
 #  updated_at      :datetime         not null
 #  password_digest :string
 #  token           :string
+#  role            :string
 #
 class UserSerializer < Blueprinter::Base
   identifier :id
 
-  fields :first_name, :last_name, :email, :created_at, :updated_at
+  fields :first_name, :last_name, :email, :role, :created_at, :updated_at
 
   view :extended do
     association :bookings, blueprint: BookingSerializer
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_22_215106) do
+ActiveRecord::Schema.define(version: 2021_07_23_213521) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 2021_07_22_215106) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
     t.string "token"
+    t.string "role"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
