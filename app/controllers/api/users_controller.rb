610c8ba7c9c1b22d7e5055d2798b1e3778625762
jmilic1 <jm51506@fer.hr>
2021-07-24 12:56:31 +0200

    rubocop disable

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -69,7 +69,7 @@ module Api
         render json: { errors: @user.errors }, status: :bad_request
       end
     end
-    # rubocop:enable Metrics/MethodLength, Metrics/AbcSize
+    # rubocop:enable Metrics/AbcSize
 
     def destroy
       @user = User.find_by(id: params[:id])
@@ -86,6 +86,7 @@ module Api
         render json: { errors: @user.errors }, status: :bad_request
       end
     end
+    # rubocop:enable Metrics/MethodLength
 
     private
 
