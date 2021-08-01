48e92d7634cef4b8f99b651f690469f672b5911d
jmilic1 <jura.milic@fer.hr>
2021-08-01 15:19:45 +0200

    Fix flight serializer

--- app/serializers/flight_serializer.rb
+++ app/serializers/flight_serializer.rb
@@ -22,7 +22,7 @@ class FlightSerializer < Blueprinter::Base
   end
 
   field :company_name do |flight|
-    flight..company.name
+    flight.company.name
   end
 
   field :current_price do |flight|
