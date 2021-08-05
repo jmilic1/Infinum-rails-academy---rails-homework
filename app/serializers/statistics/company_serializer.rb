343301e3625634417feded6c4298803b575321a3
jmilic1 <jura.milic@fer.hr>
2021-08-05 15:48:38 +0200

    Fix company serializer

--- app/serializers/statistics/company_serializer.rb
+++ app/serializers/statistics/company_serializer.rb
@@ -3,31 +3,35 @@ module Statistics
     identifier :id, name: :company_id
 
     field :total_revenue do |company|
-      company.flights.inject(0) do |acc, flight|
-        acc + flight.bookings.inject(0) do |sum, booking|
-          sum + booking.seat_price * booking.no_of_seats
-        end
-      end
+      total_revenue(company)
     end
 
     field :total_no_of_booked_seats do |company|
+      total_no_of_booked_seats(company)
+    end
+
+    field :average_price_of_seat do |company|
+      seats = total_no_of_booked_seats(company)
+      revenue = total_revenue(company)
+      if seats.zero?
+        revenue
+      else
+        revenue / seats
+      end
+    end
+
+    def self.total_no_of_booked_seats(company)
       company.flights.inject(0) do |acc, flight|
         acc + flight.bookings.inject(0) { |sum, booking| sum + booking.no_of_seats }
       end
     end
 
-    field :average_price_of_seat do |company|
-      total_revenue = company.flights.inject(0) do |acc, flight|
+    def self.total_revenue(company)
+      company.flights.inject(0) do |acc, flight|
         acc + flight.bookings.inject(0) do |sum, booking|
           sum + booking.seat_price * booking.no_of_seats
         end
       end
-
-      booked_seats = company.flights.inject(0) do |acc, flight|
-        acc + flight.bookings.inject(0) { |sum, booking| sum + booking.no_of_seats }
-      end
-
-      total_revenue / booked_seats
     end
   end
 end
