a4404747e1b39d303703d77fbbbb608907bd0527
jmilic1 <jura.milic@fer.hr>
2021-08-14 14:56:11 +0200

    Test

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -31,7 +31,7 @@ class Booking < ApplicationRecord
   def booking_overbook
     return if no_of_seats.nil? || flight.nil?
 
-    errors.add(flight.bookings.length)
+    errors.add(flight.no_of_seats)
     # return unless flight.overbooked?
 
     errors.add(:no_of_seats, 'this booking has overbooked the flight')
