587e0ca4d16de0b2a4cd61019e85f5e879293052
jmilic1 <jura.milic@fer.hr>
2021-08-15 20:44:52 +0200

    Add statistics tests

---
+++ spec/requests/api/statistics/companies_spec.rb
@@ -0,0 +1,26 @@
+RSpec.describe 'Companies Statistics API', type: :request do
+  include TestHelpers::JsonResponse
+  let!(:admin) { create(:user, token: 'admin-token', role: 'admin') }
+
+  describe 'GET /statistics/companies' do
+    before { create_list(:company, 3) }
+
+    it 'successfully returns a list of companies' do
+      get '/api/statistics/companies',
+          headers: auth_headers(admin)
+
+      expect(response).to have_http_status(:ok)
+      expect(json_body['companies'].length).to equal(3)
+    end
+
+    it 'returns required fields' do
+      get '/api/statistics/companies',
+          headers: auth_headers(admin)
+
+      puts json_body
+      expect(json_body['companies']).to all include('company_id', 'total_revenue',
+                                                    'total_no_of_booked_seats',
+                                                    'average_price_of_seats')
+    end
+  end
+end
---
+++ spec/requests/api/statistics/flights_spec.rb
@@ -0,0 +1,25 @@
+RSpec.describe 'Flights Statistics API', type: :request do
+  include TestHelpers::JsonResponse
+  let!(:admin) { create(:user, token: 'admin-token', role: 'admin') }
+
+  describe 'GET /statistics/flights' do
+    before { create_list(:flight, 3) }
+
+    it 'successfully returns a list of flights' do
+      get '/api/statistics/flights',
+          headers: auth_headers(admin)
+
+      expect(response).to have_http_status(:ok)
+      expect(json_body['flights'].length).to equal(3)
+    end
+
+    it 'returns required fields' do
+      get '/api/statistics/flights',
+          headers: auth_headers(admin)
+
+      puts json_body
+      expect(json_body['flights']).to all include('flight_id', 'revenue', 'no_of_booked_seats',
+                                                  'occupancy')
+    end
+  end
+end
