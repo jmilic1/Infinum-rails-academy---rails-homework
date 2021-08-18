e12528750fae862cf4175fafafbb96d598fcd2e2
jmilic1 <jura.milic@fer.hr>
2021-08-18 20:23:25 +0200

    Move statistics/company serializer logic to company

--- app/models/company.rb
+++ app/models/company.rb
@@ -11,4 +11,18 @@ class Company < ApplicationRecord
   has_many :flights, dependent: :destroy
 
   validates :name, uniqueness: { case_sensitive: false }, presence: true
+
+  def self.total_revenue
+    flights.inject(0) do |acc, flight|
+      acc + flight.bookings.sum do |booking|
+        booking.seat_price * booking.no_of_seats
+      end
+    end
+  end
+
+  def self.total_no_of_booked_seats
+    flights.inject(0) do |acc, flight|
+      acc + flight.bookings.sum(&:no_of_seats)
+    end
+  end
 end
--- app/serializers/statistics/company_serializer.rb
+++ app/serializers/statistics/company_serializer.rb
@@ -2,35 +2,17 @@ module Statistics
   class CompanySerializer < Blueprinter::Base
     identifier :id, name: :company_id
 
-    field :total_revenue do |company|
-      total_revenue(company)
-    end
+    field :total_revenue, &:total_revenue
 
-    field :total_no_of_booked_seats do |company|
-      total_no_of_booked_seats(company)
-    end
+    field :total_no_of_booked_seats, &:total_no_of_booked_seats
 
     field :average_price_of_seats do |company|
-      seats = total_no_of_booked_seats(company)
-      revenue = total_revenue(company).to_f
+      seats = company.total_no_of_booked_seats
+      revenue = company.total_revenue.to_f
       if seats.zero?
         revenue
       else
         revenue / seats
-      end
-    end
-
-    def self.total_no_of_booked_seats(company)
-      company.flights.inject(0) do |acc, flight|
-        acc + flight.bookings.inject(0) { |sum, booking| sum + booking.no_of_seats }
-      end
-    end
-
-    def self.total_revenue(company)
-      company.flights.inject(0) do |acc, flight|
-        acc + flight.bookings.inject(0) do |sum, booking|
-          sum + booking.seat_price * booking.no_of_seats
-        end
       end
     end
   end
